90d15bb98fff560b1df11eb58261652303e08ce9
qwelian <qwelian@qwelians-Laptop.search.charter.net>
2024-06-24 14:02:03 -0400

    this runs migrations and seeeds the db

--- prisma/seed/seed.ts
+++ prisma/seed/seed.ts
@@ -1,25 +1,51 @@
-/**
- * ! Executing this script will delete all data in your database and seed it with 10 user.
- * ! Make sure to adjust the script to your needs.
- * Use any TypeScript runner to run this script, for example: `npx tsx seed.ts`
- * Learn more about the Seed Client by following our guide: https://docs.snaplet.dev/seed/getting-started
- */
-import { createSeedClient } from "@snaplet/seed";
-
-const main = async () => {
-  const seed = await createSeedClient();
-
-  // Truncate all tables in the database
-  await seed.$resetDatabase();
-
-  // Seed the database with 10 user
-  await seed.user((x) => x(10));
-
-  // Type completion not working? You might want to reload your TypeScript Server to pick up the changes
-
-  console.log("Database seeded successfully!");
-
-  process.exit();
-};
-
-main();
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+async function main() {
+  const alice = await prisma.user.upsert({
+    where: { email: 'alice@prisma.io' },
+    update: {},
+    create: {
+      email: 'alice@prisma.io',
+      name: 'Alice',
+      posts: {
+        create: {
+          title: 'Check out Prisma with Next.js',
+          content: 'https://www.prisma.io/nextjs',
+          published: true,
+        },
+      },
+    },
+  })
+  const bob = await prisma.user.upsert({
+    where: { email: 'bob@prisma.io' },
+    update: {},
+    create: {
+      email: 'bob@prisma.io',
+      name: 'Bob',
+      posts: {
+        create: [
+          {
+            title: 'Follow Prisma on Twitter',
+            content: 'https://twitter.com/prisma',
+            published: true,
+          },
+          {
+            title: 'Follow Nexus on Twitter',
+            content: 'https://twitter.com/nexusgql',
+            published: true,
+          },
+        ],
+      },
+    },
+  })
+  console.log({ alice, bob })
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
